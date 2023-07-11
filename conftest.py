a5091b6a4e66255041b99b0a518a2127cdd90fb9
Moiseykin_PN <moiseykinp@gmail.com>
2023-07-11 15:11:30 +0500

    Adding the readme file and the starrt files of the project.

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+
+# check parameters for start
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: es or ru or another")
+
+
+# fixture for start test
+@pytest.fixture(scope="function")
+def browser(request):
+    # add language
+    language = request.config.getoption("language")
+
+    # add options for language
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+
+    # start browser
+    browser = webdriver.Chrome(chrome_options=options)
+
+    # quit browser
+    yield browser
+    browser.quit()
